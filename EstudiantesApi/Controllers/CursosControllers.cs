af2a0d765178d5594ddcfc45c464c208c77b3189
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-03-09 12:54:18 -0400

    creando las entidades

---
+++ EstudiantesApi/Controllers/CursosControllers.cs
@@ -0,0 +1,22 @@
+﻿using EstudiantesApi.Entidades;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstudiantesApi.Controllers
+{
+    [Route("api/cursos")]
+    [ApiController]
+    public class CursosControllers
+    {
+        [HttpGet]
+        public List<Cursos> Get()
+        {
+            return new List<Cursos>
+            {
+                new Cursos { Id = 1, Nombre = "Matemáticas" },
+                new Cursos { Id = 2, Nombre = "Historia" },
+                new Cursos { Id = 3, Nombre = "Ciencias" }
+            };
+        }
+    }
+}
---
+++ EstudiantesApi/Entidades/Cursos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstudiantesApi.Entidades
+{
+    public class Cursos
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre del curso es obligatorio.")]
+        public required string Nombre { get; set; } = null!;
+    }
+}
