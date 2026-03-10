387297742131d25bd7efe91541a48d63ca708d08
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-03-09 21:45:32 -0400

    entidad estudiante lista

---
+++ EstudiantesApi/Controllers/EstudiantesControllers.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EstudiantesApi.Controllers
+{
+    [Route("api/estudiantes")]
+    [ApiController]
+    public class EstudiantesControllers
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkResult();
+        }
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return new OkResult();
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id)
+        {
+            return new OkResult();
+        }
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return new OkResult();
+        }
+
+    }
+}
--- EstudiantesApi/Entidades/Estudiante.cs
+++ EstudiantesApi/Entidades/Estudiante.cs
@@ -1,15 +1,25 @@
-﻿namespace EstudiantesApi.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstudiantesApi.Entidades
 {
     public class Estudiante
     {
         int Id { get; set; }
-        string Nombre { get; set; } = null!;
-        string Apellido { get; set; } = null!;
+        [Required(ErrorMessage = "El nombre del estudiante es obligatorio.")]
+        [Range(2,60, ErrorMessage ="El campo{0} debe tener entre {1} y {2} caracteres")]
+        public required string  Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "El apellido del estudiante es obligatorio.")]
+        [Range(2, 60, ErrorMessage = "El campo{0} debe tener entre {1} y {2} caracteres")]
+        public required string Apellido { get; set; } = null!;
         string? NombrePadre { get; set; }
         string? NombreMadre { get; set; }
         string? NombreTutor { get; set; }
-        double Telefono { get; set; } 
-        string Direccion { get; set; } = null!;
+        [Required(ErrorMessage = "El telefono obligatorio.")]
+        [Range(1000000000, 9999999999, ErrorMessage = "El campo {0} debe ser un número de teléfono válido de 10 dígitos.")]
+        public required double Telefono { get; set; }
+        [Required(ErrorMessage = "La dirección es obligatoria.")]
+        [Range(5, 100, ErrorMessage = "El campo {0} debe tener entre {1} y {2} caracteres.")]
+        public required string Direccion { get; set; } = null!;
         string? Foto { get; set; } 
         string? ActaNacimiento { get; set; }
 
